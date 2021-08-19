b677e86dfc7a4580f4b9a0fec43c2425256054c7
Ian Jackson <ijackson@chiark.greenend.org.uk>
2021-08-19 11:54:01 +0100

    conf.py: Fix some leftover Otter stuff
    Signed-off-by: Ian Jackson <ijackson@chiark.greenend.org.uk>

--- docs/conf.py
+++ docs/conf.py
@@ -183,7 +183,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'otter', 'Rust for the Polyglot Programmer',
+    (master_doc, 'rust-polyglot', 'Rust for the Polyglot Programmer',
      [author], 1)
 ]
 
